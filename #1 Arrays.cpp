e61b9d53e1c5ef8a1fbfbd988f8a647742ad7f74
theshamiksinha <shamiksinha2003@outlook.com>
2024-01-04 17:37:54 +0530

    append and insert

--- #1 Arrays.cpp
+++ #1 Arrays.cpp
@@ -21,6 +21,28 @@ void Display( struct Array arr){ // Function to display an array;
 
 }
 
+void Append(struct Array *arr, int x){ // Appending a value x to the end of the array
+
+    if(arr->length < arr->size){
+
+        arr->A[arr->length++] = x;
+    }
+}
+
+void Insert(struct Array *arr, int index, int x){
+
+    if(index>=0 && index <= arr->length){
+
+        for(int i = arr->length; i>index; i--){
+
+            arr->A[i] = arr->A[i-1];
+        }
+
+        arr->A[index] = x;
+        arr-> length++;
+    }
+}
+
 
 int main(){
 
